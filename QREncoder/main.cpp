4344832df345aa405d75615a18a7a10686481dd1
machinematrix <32741597+machinematrix@users.noreply.github.com>
2021-03-21 02:22:55 -0300

    Modified command line interface.

--- QREncoder/main.cpp
+++ QREncoder/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include <fstream>
 #include <iostream>
-#include <vector>
 #include <unordered_map>
 #include <regex>
+#include <optional>
 #include "Image.h"
 #include "QRCode.h"
 
@@ -34,61 +34,88 @@ int main(int argc, char **argv)
 	using std::cout;
 	using std::cerr;
 	using std::endl;
-	int result = 0;
+	std::ofstream output;
+	std::regex versionFormat("-(M)?([[:digit:]]{1,2})-([LMQH])");
+	std::cmatch results;
+	std::unordered_map<char, QR::ErrorCorrectionLevel> levels = {
+		{ 'L', QR::ErrorCorrectionLevel::L },
+		{ 'M', QR::ErrorCorrectionLevel::M },
+		{ 'Q', QR::ErrorCorrectionLevel::Q },
+		{ 'H', QR::ErrorCorrectionLevel::H }
+	};
+	std::unordered_map<std::string_view, QR::Mode> modes = {
+		{ "-numeric", QR::Mode::NUMERIC },
+		{ "-alpha", QR::Mode::ALPHANUMERIC },
+		{ "-byte", QR::Mode::BYTE }
+	};
+	
+	std::string message;
+	std::optional<QR::SymbolType> type;
+	std::optional<std::uint8_t> version;
+	std::optional<QR::ErrorCorrectionLevel> level;
+	std::vector<QR::ModeRange> modeRanges;
 
-	if (argc == 4)
+	for (int i = 1; i < argc - 1; ++i)
 	{
-		std::ofstream output;
-		std::regex versionFormat("(M)?([[:digit:]]{1,2})-([LMQH])");
-		std::cmatch results;
+		decltype(modes)::iterator modeIt;
+		std::string_view argument(argv[i]);
 
-		if (std::regex_match(argv[2], results, versionFormat))
+		if ((modeIt = modes.find(argument)) != modes.end())
 		{
-			std::unordered_map<char, QR::ErrorCorrectionLevel> levels = {
-				{ 'L', QR::ErrorCorrectionLevel::L },
-				{ 'M', QR::ErrorCorrectionLevel::M },
-				{ 'Q', QR::ErrorCorrectionLevel::Q },
-				{ 'H', QR::ErrorCorrectionLevel::H }
-			};
-			QR::SymbolType type = results[1].matched ? QR::SymbolType::MICRO_QR : QR::SymbolType::QR;
-			std::uint8_t version = std::stoul(results[2]);
+			auto oldSize = message.size();
 
-			output.open(argv[3], std::ios_base::binary);
-
-			if (output.is_open())
-				output << QRToBMP(QR::Encode(argv[1], type, version, levels.at(results[3].str().front()), QR::Mode::BYTE), 8);
-			else
-			{
-				cerr << "Could not open file" << endl;
-				result = -1;
-			}
+			message += argv[i + 1];
+			modeRanges.emplace_back(modeIt->second, oldSize, message.size());
+			++i;
 		}
-		else
-			if (std::string_view(argv[2]) == "M1")
-			{
-				std::ofstream output(argv[3], std::ios_base::binary);
-
-				if (output.is_open())
-					output << QRToBMP(QR::Encode(argv[1], QR::SymbolType::MICRO_QR, 1, QR::ErrorCorrectionLevel::ERROR_DETECTION_ONLY, QR::Mode::BYTE), 8);
-				else
-				{
-					cerr << "Could not open file" << endl;
-					result = -1;
-				}
-			}
-			else
-			{
-				cerr << "Invalid version" << endl;
-				result = -1;
-			}
+		else if (argument == "-output")
+		{
+			output.open(argv[i + 1], std::ios_base::binary);
+			++i;
+		}
+		else if (argument == "-M1")
+		{
+			type = QR::SymbolType::MICRO_QR;
+			version = 1;
+			level = QR::ErrorCorrectionLevel::ERROR_DETECTION_ONLY;
+		}
+		else if (std::regex_match(argument.data(), results, versionFormat))
+		{
+			type = results[1].matched ? QR::SymbolType::MICRO_QR : QR::SymbolType::QR;
+			version = static_cast<std::uint8_t>(std::stoul(results[2]));
+			level = levels.at(results[3].str().front());
+		}
 	}
-	else
+
+	if (argc == 1)
 	{
-		cout	<< "Usage: " << argv[0] << " message [M]V-E output_file\n"
-				<< "M: Indicates that the output will be a Micro QR symbol\n"
-				<< "V: Indicates version number. Max is 40 for QR symbols and 4 for Micro QR symbols\n"
-				<< "E: Error correction levels. Valid values are L, M, Q, H" << endl;
+		cout << "Usage: " << argv[0] << " -numeric|alpha|byte message -[M]V-E -output filename\n"
+			<< "M: Indicates that the output will be a Micro QR symbol\n"
+			<< "V: Indicates version number. Max is 40 for QR symbols and 4 for Micro QR symbols\n"
+			<< "E: Error correction levels. Valid values are L, M, Q, H" << endl;
+
+		return 0;
 	}
 
-	return result;
+	if (!output.is_open())
+	{
+		cerr << "Could not open output file" << endl;
+		return -1;
+	}
+
+	if (message.empty())
+	{
+		cerr << "You must supply a message" << endl;
+		return -1;
+	}
+
+	if (!type.has_value())
+	{
+		cerr << "You must specify symbol type, version and error correction level" << endl;
+		return -1;
+	}
+
+	output << QRToBMP(QR::Encode(message, type.value(), version.value(), level.value(), modeRanges), 4);
+
+	return 0;
 }
